9f84be974ffdb5a53c830aeea68ff489aa652981
Naveen <er.naveen1911@gmail.com>
2022-12-19 22:39:21 +0530

    Kulfy SDK initial commit

---
+++ Tests/KulfySDKTests/KulfySDKTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import KulfySDK
+
+final class KulfySDKTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(KulfySDK().text, "Hello, World!")
+    }
+}
---
+++ build/KulfyFramwork.xcframework/ios-arm64/KulfyFramwork.framework/Headers/KulfyFramwork.h
@@ -0,0 +1,18 @@
+//
+//  KulfyFramwork.h
+//  KulfyFramwork
+//
+//  Created by Naveen Kumar on 14/12/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KulfyFramwork.
+FOUNDATION_EXPORT double KulfyFramworkVersionNumber;
+
+//! Project version string for KulfyFramwork.
+FOUNDATION_EXPORT const unsigned char KulfyFramworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KulfyFramwork/PublicHeader.h>
+
+
